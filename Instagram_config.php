2dc08ba25b89873e0183a30d80aaced534b426e8
Afafabdb <91419650+Afafabdb@users.noreply.github.com>
2021-10-01 16:47:49 +0530

    Update Instagram_config.php

--- Instagram_config.php
+++ Instagram_config.php
@@ -1,43 +1,23 @@
+
 <?php
-
  
-
 $searchtag = array(
-
     'instagram',
-
     'aquamarine',
-
     'wifi',
-
     'lovestory',
-
 );
-
  
-
  
-
 $hello = array(
-
     'Hi',
-
     'Hello',
-
     '',
-
 );
-
  
-
 $praise = array(   
-
     ', so nice.',
-
     ', great stuff.',
-
     ', cool picture',
-
 );
-
 ?>
