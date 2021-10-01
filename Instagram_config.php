da381fed75f98d29e76091954547c092209c41e8
Afafabdb <91419650+Afafabdb@users.noreply.github.com>
2021-10-01 16:42:42 +0530

    Create Instagram_config.php

---
+++ Instagram_config.php
@@ -0,0 +1,43 @@
+<?php
+
+ 
+
+$searchtag = array(
+
+    'instagram',
+
+    'aquamarine',
+
+    'wifi',
+
+    'lovestory',
+
+);
+
+ 
+
+ 
+
+$hello = array(
+
+    'Hi',
+
+    'Hello',
+
+    '',
+
+);
+
+ 
+
+$praise = array(   
+
+    ', so nice.',
+
+    ', great stuff.',
+
+    ', cool picture',
+
+);
+
+?>
